8558fe57427e8327ef6812002d9eb7df6a30e259
Luis Dario Rodríguez <luisdario500@gmail.com>
2025-08-08 21:02:08 -0600

    Actualizacion 1.15

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ require("dotenv").config({
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 app.engine(
   "handlebars",
   expressHDS({
